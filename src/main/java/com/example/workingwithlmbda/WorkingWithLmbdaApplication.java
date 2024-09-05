e4054e546028bcc9157fca774dbea7e9f67de148
Masoomeh Aslani <masiw.rad7@gmail.com>
2024-09-05 16:35:57 +0330

    Initial commit

---
+++ src/main/java/com/example/workingwithlmbda/WorkingWithLmbdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.workingwithlmbda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithLmbdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingWithLmbdaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/workingwithlmbda/model/Person.java
@@ -0,0 +1,2 @@
+package com.example.workingwithlmbda.model;public class Person {
+}
